b5c7089f7dab55eac652b32ef280fbb486eed392
Adam Michalak <adam_michalak@epam.com>
2018-11-28 12:35:46 +0100

    initialize GameFlowAPI object and method that starts program

--- src/main/java/pl/michalak/adam/OX.java
+++ src/main/java/pl/michalak/adam/OX.java
@@ -1,11 +1,15 @@
 package pl.michalak.adam;
+
+import pl.michalak.adam.gameflow.GameFlowAPI;
+
 /**
  * Main class of the program.
  *
  * @author Adam Michalak
  */
-class OX {
+public class OX {
 public static void main(String[] args) {
-
+        GameFlowAPI gameFlowAPI = new GameFlowAPI();
+        gameFlowAPI.start();
     }
 }
