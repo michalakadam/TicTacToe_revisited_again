0df19147d93cacb5df748c81a94a4dc73f4a9869
Adam Michalak <adam_michalak@epam.com>
2018-11-28 14:14:40 +0100

    add validator for integer values

---
+++ src/main/java/pl/michalak/adam/input/IntInputValidator.java
@@ -0,0 +1,38 @@
+package pl.michalak.adam.input;
+
+import pl.michalak.adam.output.OutputAPI;
+
+import java.util.InputMismatchException;
+
+class IntInputValidator {
+    private final OutputAPI outputAPI;
+    private final InputReader inputReader;
+    
+    IntInputValidator(OutputAPI outputAPI, InputReader inputReader){
+        this.outputAPI = outputAPI;
+        this.inputReader = inputReader;
+    }
+    private int checkInputFromPlayer(){
+        try {
+            return inputReader.getInt();
+        }
+        catch(InputMismatchException inputMismatchException) {
+            outputAPI.printFromResourceBundle("wrongValue");
+            inputReader.getString();
+            return checkInputFromPlayer();
+        }
+    }
+    private boolean inputIsInRange(int input, int min, int max){
+        return input >= min && input <= max;
+    }
+    public int getIntInputFromPlayer(String message, int min, int max){
+        outputAPI.printFromResourceBundle(message);
+        int input = checkInputFromPlayer();
+        if (inputIsInRange(input, min, max)) {
+            return input;
+        }
+        outputAPI.printFromResourceBundle(String.format("numberOutOfRange", min, max));
+        return getIntInputFromPlayer(message, min, max);
+    }
+}
+
---
+++ src/test/java/pl/michalak/adam/input/IntInputValidatorTest.java
@@ -0,0 +1,112 @@
+package pl.michalak.adam.input;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import pl.michalak.adam.output.OutputAPI;
+
+import java.io.ByteArrayInputStream;
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+
+import static org.testng.Assert.assertEquals;
+
+public class IntInputValidatorTest {
+    InputReader inputReader;
+    IntInputValidator intInputValidator;
+    OutputAPI outputAPI;
+    @BeforeMethod
+    public void setUp(){
+        outputAPI = new OutputAPI(new PrintStream(System.out));
+    }
+    @DataProvider
+    public static Object[] numbers(){
+        return new Object[]{
+                -10000,
+                -1000,
+                -999,
+                -100,
+                -10,
+                -5,
+                5,
+                10,
+                101,
+                999,
+                1000,
+                10000
+        };
+    }
+
+    @Test(expectedExceptions = NoSuchElementException.class, dataProvider="numbers")
+    public void inputShouldBeOutOfRange(int number) {
+        //given
+        String data = Integer.toString(number);
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        intInputValidator = new IntInputValidator(outputAPI, inputReader);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(intInputValidator.getIntInputFromPlayer(message, min, max), number);
+    }
+
+    @Test
+    public void inputJustInRangeFromTheLeft(){
+        //given
+        String data = "-4";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        intInputValidator = new IntInputValidator(outputAPI, inputReader);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(intInputValidator.getIntInputFromPlayer(message, min, max), -4);
+    }
+
+    @Test
+    public void inputJustInRangeFromTheRight(){
+        //given
+        String data = "4";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        intInputValidator = new IntInputValidator(outputAPI, inputReader);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(intInputValidator.getIntInputFromPlayer(message, min, max), 4);
+    }
+    @Test(expectedExceptions = NoSuchElementException.class)
+    public void inputEnterShouldThrowException() {
+        //given
+        String data = "";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        intInputValidator = new IntInputValidator(outputAPI, inputReader);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(intInputValidator.getIntInputFromPlayer(message, min, max), 0);
+    }
+    @Test()
+    public void inputSeparatedByWhiteSpaceShouldReadOnlyFirstNumber() {
+        //given
+        String data = "-2 2";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        intInputValidator = new IntInputValidator(outputAPI, inputReader);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(intInputValidator.getIntInputFromPlayer(message, min, max), -2);
+    }
+}
