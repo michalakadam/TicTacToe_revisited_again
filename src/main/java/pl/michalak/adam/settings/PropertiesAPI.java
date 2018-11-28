4c73ff9b30982e0a7a3525acbe4dce43773240d2
Adam Michalak <adam_michalak@epam.com>
2018-11-28 16:25:24 +0100

    add method that sets locale for RB when game starts

--- src/main/java/pl/michalak/adam/settings/PropertiesAPI.java
+++ src/main/java/pl/michalak/adam/settings/PropertiesAPI.java
@@ -1,12 +1,17 @@
 package pl.michalak.adam.settings;
 
-class PropertiesAPI {
+import java.util.Locale;
+
+public class PropertiesAPI {
     private final WinningCondition winningCondition;
 
     public PropertiesAPI(int winningCondition){
         this.winningCondition = new WinningCondition(winningCondition);
     }
 
+    public void setLocale(String localeCode){
+        Locale.setDefault(new Locale(localeCode));
+    }
     public int getWinningConditionForThisGame(){
         return winningCondition.getWinningCondition();
     }
