85e08f1825a1ad5265cfa8edef346db0d4ac4bab
Adam Michalak <adam_michalak@epam.com>
2018-11-29 08:48:05 +0100

    move validators tests to inputAPI test suite

--- src/test/java/pl/michalak/adam/input/InputAPITest.java
+++ src/test/java/pl/michalak/adam/input/InputAPITest.java
@@ -11,14 +11,119 @@ import java.util.NoSuchElementException;
 
 import static org.testng.Assert.*;
 
-public class StringInputValidatorTest {
+public class InputAPITest {
     InputReader inputReader;
-    IntInputValidator intInputValidator;
+    InputAPI inputAPI;
     OutputAPI outputAPI;
     @BeforeMethod
     public void setUp(){
         outputAPI = new OutputAPI(new PrintStream(System.out));
     }
+    @DataProvider
+    public static Object[] numbers(){
+        return new Object[]{
+                -10000,
+                -1000,
+                -999,
+                -100,
+                -10,
+                -5,
+                5,
+                10,
+                101,
+                999,
+                1000,
+                10000
+        };
+    }
+
+    @Test(expectedExceptions = NoSuchElementException.class, dataProvider="numbers")
+    public void inputShouldBeOutOfRange(int number) {
+        //given
+        String data = Integer.toString(number);
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        inputAPI = new InputAPI(outputAPI);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(inputAPI.getIntInputFromPlayer(message, min, max), number);
+    }
+
+    @Test
+    public void inputJustInRangeFromTheLeft(){
+        //given
+        String data = "-4";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        inputAPI = new InputAPI(outputAPI);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(inputAPI.getIntInputFromPlayer(message, min, max), -4);
+    }
+
+    @Test
+    public void inputJustInRangeFromTheRight(){
+        //given
+        String data = "4";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        inputAPI = new InputAPI(outputAPI);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(inputAPI.getIntInputFromPlayer(message, min, max), 4);
+    }
+    @Test(expectedExceptions = NoSuchElementException.class)
+    public void inputEnterShouldThrowException() {
+        //given
+        String data = "";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        inputAPI = new InputAPI(outputAPI);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(inputAPI.getIntInputFromPlayer(message, min, max), 0);
+    }
+    @Test()
+    public void inputSeparatedByWhiteSpaceShouldReadOnlyFirstNumber() {
+        //given
+        String data = "-2 2";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        inputAPI = new InputAPI(outputAPI);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(inputAPI.getIntInputFromPlayer(message, min, max), -2);
+    }
+    @Test(expectedExceptions = NoSuchElementException.class)
+    public void inputNotIntThrowsException(){
+        //given
+        String data = "SomeWordsHereNotInts";
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        inputReader = new InputReader(System.in);
+        inputAPI = new InputAPI(outputAPI);
+        //when
+        String message = "testMessage";
+        int min = -4;
+        int max = 4;
+        //then
+        assertEquals(inputAPI.getIntInputFromPlayer(message, min, max), 4);
+    }
+
     @Test(expectedExceptions = NoSuchElementException.class)
     public void inputIsWhiteSpaceShouldNotWork() {
         //given
