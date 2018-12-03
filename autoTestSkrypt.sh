40e212181654e059d650920d1c6fc10da727ee72
Adam Michalak <adam_michalak@epam.com>
2018-12-03 07:09:22 +0100

    provide proper file names to write to

--- autoTestSkrypt.sh
+++ autoTestSkrypt.sh
@@ -10,8 +10,8 @@ touch $LOG_FILE3
 (echo 2 1 gracz1 2 2 gracz2 1 5 1 3 2 7 y 1 2 3 4 5 9 6 7 8 y 1 5 7 4 3 6 n) | java -jar target/OX.java.academy-2.0.jar | tee ${LOG_FILE1}
 
 #LOG_FILE2
-(echo 4 1 2 1 player1 2 2 player2 3 1 5 3 2 4 1 2 3 7 8 12 13 17 18 y 2 7 8 13 14 19 20 y 6 11 7 12 8 13 9 14 y 2 7 8 13 14 19 20 n n) | java -jar target/OX.java.academy-2.0.jar | tee ${LOG_FILE1}
+(echo 4 1 2 1 player1 2 2 player2 3 1 5 3 2 4 1 2 3 7 8 12 13 17 18 y 2 7 8 13 14 19 20 y 6 11 7 12 8 13 9 14 y 2 7 8 13 14 19 20 n n) | java -jar target/OX.java.academy-2.0.jar | tee ${LOG_FILE2}
 
 #LOG_FILE3
-(echo 2 1 gracz1 2 2 gracz2 3 1 9 3 2 5 1 14 23 24 33 34 43 44 53 54 n n) | java -jar target/OX.java.academy-2.0.jar | tee ${LOG_FILE1}
+(echo 2 1 gracz1 2 2 gracz2 3 1 9 3 2 5 1 14 23 24 33 34 43 44 53 54 n n) | java -jar target/OX.java.academy-2.0.jar | tee ${LOG_FILE3}
 
