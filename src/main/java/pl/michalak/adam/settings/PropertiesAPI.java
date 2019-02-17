7570dd5cea2048d9d3c48c981280fa4565ea69da
Adam Michalak <adam_michalak@epam.com>
2019-02-17 17:01:28 +0100

    add proper Javadoc

--- src/main/java/pl/michalak/adam/settings/PropertiesAPI.java
+++ src/main/java/pl/michalak/adam/settings/PropertiesAPI.java
@@ -2,12 +2,10 @@ package pl.michalak.adam.settings;
 /**
  * This public API handles game properties
  * like locale, size of the board and winning condition.
- * <p>
  * Locale is required when language of the program is set.
  * Then resource bundle is initialized with the new default locale.
  * Winning condition is length of the winning sequence required to win the game.
  * Board size is length of the board side on which the game will be played.
- * <p/>
  * @author Adam Michalak
 */
 public class PropertiesAPI {
@@ -25,7 +23,6 @@ public class PropertiesAPI {
      * This method sets new locale as default.
      * This happens when resource bundle is initialized
      * or language is changed during the intro game phase.
-     *
      * @param localeCode1 is a String representation of a language code.
      * @param localeCode2 is a String representation of a country code
     */
